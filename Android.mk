5f7d390ce2022c0f0bf8d15585f64da1f41a5449
RusJJ <rusjj1337@gmail.com>
2025-01-19 09:47:33 +0300

    Woopsies, forgotten 64 at the name

--- Android.mk
+++ Android.mk
@@ -2,7 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_MODULE    := RadarShapeR
+ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+	LOCAL_MODULE := RadarShapeR
+else
+	LOCAL_MODULE := RadarShapeR64
+endif
 LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp
 LOCAL_CFLAGS += -Ofast -mfloat-abi=softfp -DNDEBUG
 LOCAL_LDLIBS += -llog
